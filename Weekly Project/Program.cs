12a0ff7c0ba77ab16452f2d8cb7506812591d830
Jetpreeya Yawichai <84719740+Jetpreeya@users.noreply.github.com>
2022-04-28 10:58:37 +0200

    Language Integrated Query (LINQ) in C#

--- Weekly Project/Program.cs
+++ Weekly Project/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -49,11 +49,12 @@ namespace CheckPoint2
             Array.Resize(ref myCategory, index);
             Console.WriteLine("My Category List");
             Console.WriteLine("---------------");
+            Console.WriteLine("Category".PadRight(10) + "Brand".PadRight(10) + "Price".PadRight(10));
             for (int i = 0; i < myCategory.Length; i++)
             {
                 Console.WriteLine(myCategory[i]);
             }
-            //Show the products list
+            //Show the Products list
             Array.Resize(ref myProducts, index);
             Console.WriteLine("My Products List");
             Console.WriteLine("---------------");
@@ -62,94 +63,58 @@ namespace CheckPoint2
                 Console.WriteLine(myProducts[i]);
             }
 
-            //Show the price list
+            //Show the Price list
             Array.Resize(ref myPrice, index);
             Console.WriteLine("My Price List");
             Console.WriteLine("---------------");
             for (int i = 0; i < myPrice.Length; i++)
             {
                 Console.WriteLine(myPrice[i]);
-
             }
 
-
             {
-                Console.WriteLine("My Products!");
-                List<Products> cars = new List<Products>();
+                List<Products> products = new List<Products>();
                 Products products1 = new Products("Food","Rice", 22);
-                cars.Add(products1);
+                products.Add(products1);
 
-                cars.Add(new Products("Car", "Saab", 19500));
+                products.Add(new Products("Car", "Saab", 19500));
 
                 List<Products> extraCars = new List<Products>
             {
                 new Products("Fruit", "Orange", 30),
                 new Products("Fruit", "Grape", 45)
             };
-
-                cars.AddRange(extraCars); //can add more than one
-
-                cars.AddRange(new List<Products>
+                products.AddRange(extraCars); //can add more than one
+                products.AddRange(new List<Products>
             {
                 new Products("Kichken","Spoon", 210),
-                new Products("Animal","Duck", 170)
+                new Products("Animal","Duck", 170),
+                new Products("Room","Bed", 1650)
             });
 
-                int highPrice = cars.Max(car => car.Price);
-                int lowPrice = cars.Min(car => car.Price);
-                double averagePrice = cars.Average(car => car.Price);
-                int sumOfAllPrice = cars.Sum(car => car.Price);
-                int countCars = cars.Count();
+                int highPrice = products.Max(product => product.Price);
+                int lowPrice = products.Min(product => product.Price);
+                double averagePrice = products.Average(product => product.Price);
+                int sumOfAllPrice = products.Sum(product => product.Price);
+                int countCars = products.Count();
+                int countVolvo = products.Where(product => product.Category.Contains("Fruit")).Count();
 
-                int countVolvo = cars.Where(car => car.Category.Contains("Fruit")).Count();
+                Console.WriteLine("___________________________________");
+                Console.WriteLine("The products list - Sorted by Price");
+                Console.WriteLine("___________________________________");
 
-
-                //Console.WriteLine("My Product - Unsorted");
-                Console.WriteLine("____________________________");
-                Console.WriteLine("Category".PadRight(10)+ "Brand".PadRight(10) + "Price".PadRight(10));
-                Console.WriteLine("____________________________");
-                foreach (Products car in cars)
+                List<Products> sortedProduct = products.OrderBy(product => product.Price).ToList();
+                Console.WriteLine("Category".PadRight(10) + "|" + "Brand".PadRight(10) + "|"  + "Price".PadRight(10));
+                foreach (Products product in sortedProduct)
                 {
-                    Console.WriteLine(car.Category.PadRight(10)+ car.Brand.PadRight(10) + car.Price.ToString().PadRight(10));
-                }
-                // Will be continouse 27/04/2022
-                Console.WriteLine("Total Sum is ");
-
-                Console.WriteLine("____________________________");
-                Console.WriteLine("My Cars - Sorted by Brand");
-                Console.WriteLine("____________________________");
-
-                List<Products> sortedCars = cars.OrderBy(car => car.Brand).ToList();
-                Console.WriteLine("Category".PadRight(10) + "Brand".PadRight(10) + "Price".PadRight(10));
-                foreach (Products car in sortedCars)
-                {
-                    Console.WriteLine(car.Category.PadRight(10) + car.Brand.PadRight(10) + car.Price.ToString().PadRight(10));
+                    Console.WriteLine(product.Category.PadRight(10) +"|"+ product.Brand.PadRight(10) + "|" + product.Price.ToString().PadRight(10));
                 }
 
-                Console.WriteLine("____________________________");
-                Console.WriteLine("My product - Filtered by Price between 1-300");
-                Console.WriteLine("____________________________");
-
-                List<Products> filteredCars = cars.Where(car => car.Price >= 1 && car.Price <= 300).ToList();
-                Console.WriteLine("Category".PadRight(10) + "Brand".PadRight(10) + "Price".PadRight(10));
-                foreach (Products car in filteredCars)
                 {
-                    Console.WriteLine(car.Category.PadRight(10) + car.Brand.PadRight(10) + car.Price.ToString().PadRight(10));
+                Console.WriteLine("___________________________________");
+                Console.WriteLine("Total Sum is " + products.Sum(car => car.Price)); // Summary process
                 }
 
-                Console.WriteLine("____________________________");
-                Console.WriteLine("The sum of all products");
-                Console.WriteLine("____________________________");
-
-                //Will be co
-                List<Products> SumCars = cars;
-                Console.WriteLine("Price".PadRight(10));
-                foreach (Products car in SumCars)
-                {
-                    Console.WriteLine(car.Price.ToString().PadRight(10));
-                }
-
-                Console.ReadLine();
             }
         }
         //class /*Category*/
