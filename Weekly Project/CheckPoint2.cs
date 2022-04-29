4f195c6e3329b98bbb5da736b4311ad44c9ea324
Jetpreeya Yawichai <84719740+Jetpreeya@users.noreply.github.com>
2022-04-29 15:02:36 +0200

    Now Can add object to <list>

--- Weekly Project/CheckPoint2.cs
+++ Weekly Project/CheckPoint2.cs
@@ -2,152 +2,56 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace CheckPoint2
+namespace Friday_CheckPoint2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            List <Products> Pr =new List <Products> ();
             Console.WriteLine("Add a product and exit with q!");
-            //List Level 1 
-            int index = 0;
-            string[] myCategory = new string[10];
-            string[] myProducts = new string[10];
-            int[] myPrice = new int[10];
             while (true)
             {
-                { 
                 Console.Write("Add a category:");
-                string data = Console.ReadLine();
-                if (data.ToLower().Trim() == "q") //exit with type q
+                string category = Console.ReadLine();
+                if (category.ToLower().Trim() == "q") //exit with type q
                 {
                     break;
-                }
-                myCategory[index] = data;
-                index++;
-                }
-                { 
+                }                
                 Console.Write("Add a product:");
-                string product = Console.ReadLine();
-                if (product.ToLower().Trim() == "q") //exit with type q
+                string brand = Console.ReadLine();
+                if (brand.ToLower().Trim() == "q") //exit with type q
                 {
                     break;
                 }
-                myProducts[index] = product;
-                index++;
-                }
-                { 
+
                 Console.Write("Add a price:");
-                string Products = Console.ReadLine();
-                //int price = Convert.ToInt32(Console.ReadLine());
-                //Check that user input price as a number or not   //Level 3 
-                bool isInt = int.TryParse(Products, out int prices);
-                if (isInt)
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine($"{Products} is a number");
-                    Array.Resize(ref myProducts, index + 1);
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine($"{Products} is NOT a number");
-                }
-                Console.ResetColor();
-                if (Products.ToLower().Trim() == "q") //exit with type q
+                string price =Console.ReadLine();
+                if (price.ToLower().Trim() == "q") //exit with type q
                 {
                     break;
                 }
-                }
 
-                // input then show information from user
-                Array.Resize(ref myCategory, index);
-                Console.WriteLine("My Category List");
-                Console.WriteLine("---------------");
+                Pr.Add(new Products(category, brand, int.Parse(price)));
+
+                Console.WriteLine("____________________________");
                 Console.WriteLine("Category".PadRight(10) + "Brand".PadRight(10) + "Price".PadRight(10));
-                for (int i = 0; i < myCategory.Length; i++)
+                Console.WriteLine("____________________________");
+                foreach (Products item in Pr)
                 {
-                    Console.WriteLine(myCategory[i]);
-                }
-                //Show the Products list
-                Array.Resize(ref myProducts, index);
-                Console.WriteLine("My Products List");
-                Console.WriteLine("---------------");
-                for (int i = 0; i < myProducts.Length; i++)
-                {
-                    Console.WriteLine(myProducts[i]);
+                    Console.WriteLine(item.Category.PadRight(10) + item.Brand.PadRight(10) + item.Price.ToString().PadRight(10));
                 }
 
-                //Show the Price list
-                Array.Resize(ref myPrice, index);
-                Console.WriteLine("My Price List");
-                Console.WriteLine("---------------");
-                for (int i = 0; i < myPrice.Length; i++)
+                //Sum of the products
+                Console.WriteLine("____________________________");
+                Console.WriteLine("The sum of all products");
+                Console.WriteLine("Price".PadRight(10));
+                foreach (Products item in Pr)
                 {
-                    Console.WriteLine(myPrice[i]);
+                    Console.WriteLine(item.Price.ToString().PadRight(10));
                 }
-
-                //Show the Products list
-            }
-
-            //Show the Products list
-            {
-                List<Products> products = new List<Products>();
-                Products products1 = new Products("Food","Rice", 28);
-                products.Add(products1);
-
-                products.Add(new Products("Car", "Saab", 19500));
-
-                List<Products> extraCars = new List<Products>
-            {
-                new Products("Fruit", "Orange", 30),
-                new Products("Fruit", "Grape", 45)
-            };
-                products.AddRange(extraCars); //can add more than one
-                products.AddRange(new List<Products>
-            {
-                new Products("Kichken","Spoon", 210),
-                new Products("Animal","Duck", 170),
-                new Products("Room","Bed", 1650)
-            });
-
-                int highPrice = products.Max(product => product.Price);
-                int lowPrice = products.Min(product => product.Price);
-                double averagePrice = products.Average(product => product.Price);
-                int sumOfAllPrice = products.Sum(product => product.Price);
-                int countCars = products.Count();
-                int countVolvo = products.Where(product => product.Category.Contains("Fruit")).Count();
-
-                Console.WriteLine("___________________________________");
-                Console.WriteLine("The products list - Sorted by Price");
-                Console.WriteLine("___________________________________");
-
-                List<Products> sortedProduct = products.OrderBy(product => product.Price).ToList();
-                Console.WriteLine("Category".PadRight(10) + "|" + "Brand".PadRight(10) + "|"  + "Price".PadRight(10));
-                foreach (var product in sortedProduct) 
-                {
-                    Console.WriteLine(product.Category.PadRight(10) +"|"+ product.Brand.PadRight(10) + "|" + product.Price.ToString().PadRight(10));
-                }
-
-                {
-                Console.WriteLine("___________________________________");
-                Console.WriteLine("Total Sum is " + products.Sum(product => product.Price)); // Summary process Level 2 
-                Console.WriteLine("___________________________________");
-                }
-                Console.WriteLine("Press enter to continue...");
-                Console.ReadLine();
-
             }
         }
-        //class /*Category*/
-        //{
-        //    public Category(string type)
-        //    {
-        //        Type = type;
-        //    }
-
-        //    public string Type { get; set; }
-        //}
         class Products
         {
             public Products(string category, string brand, int price)
@@ -156,11 +60,9 @@ namespace CheckPoint2
                 Brand = brand;
                 Price = price;
             }
-
             public string Category { get; set; }
             public string Brand { get; set; }
             public int Price { get; set; }
         }
     }
 }
-
