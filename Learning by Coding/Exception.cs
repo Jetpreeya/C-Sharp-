7ce771026c550746ed613eab3497d87f98649195
Jetpreeya Yawichai <84719740+Jetpreeya@users.noreply.github.com>
2022-05-03 13:45:29 +0200

    Try Catch Final

---
+++ Learning by Coding/Exception.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Exception220503
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DoSomethings();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Catching an exeption from method(){0}", ex.Message);
+            }
+        }
+
+        private static void DoSomethings()
+        {
+            try
+            {
+                Console.WriteLine("Enter a number");
+                string input = Console.ReadLine();
+                int value = Convert.ToInt32(input);
+            }
+
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("The number cannot devide by 0");
+            }
+
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Index Out Of Range Exception");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Format Exception");
+            }
+            catch (OverflowException)
+            {
+                throw new Exception("Format should be a int!");
+            }
+            catch (ArithmeticException)
+            {
+                Console.WriteLine("Arithmetic Exception");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+
+            }
+            finally
+            {
+                Console.WriteLine("Program executed!");
+            }
+        }
+    }
+}
