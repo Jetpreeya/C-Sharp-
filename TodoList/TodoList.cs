d7db895e9e819c72b6b47340d979f4364dfd209b
Jetpreeya Yawichai <84719740+Jetpreeya@users.noreply.github.com>
2022-05-04 14:17:51 +0200

    Todo List with C#

---
+++ TodoList/TodoList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TodoList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TODO List");
+            Console.WriteLine("You have X tasks todo and Y tasks are done!");
+            Console.WriteLine("Pick an option :");
+        }
+    }
+}
