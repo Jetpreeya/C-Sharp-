6eea99ac72c8d80fbefe25551a8dbee1f6963b62
Jetpreeya Yawichai <84719740+Jetpreeya@users.noreply.github.com>
2022-05-09 10:45:26 +0200

    Update Todo List

--- TodoList/TodoList.cs
+++ TodoList/TodoList.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace TodoList
 {
@@ -9,6 +9,10 @@ namespace TodoList
             Console.WriteLine("Welcome to TODO List");
             Console.WriteLine("You have X tasks todo and Y tasks are done!");
             Console.WriteLine("Pick an option :");
+            Console.WriteLine("(1) Show task list (by date or project) ");
+            Console.WriteLine("(2) Add New Task ");
+            Console.WriteLine("(3) Edit Task(update, mark as done, remove) ");
+            Console.WriteLine("(4) Save and Quit ");
         }
     }
 }
