db712386277633eeaac09acecede103f1deb7e0e
Jetpreeya Yawichai <84719740+Jetpreeya@users.noreply.github.com>
2022-06-03 10:22:31 +0200

    Level 2 use List

--- AssetTrackingDatabase/AssetTrackingDatabase/Program.cs
+++ AssetTrackingDatabase/AssetTrackingDatabase/Program.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Data.SqlClient;
+using AssetTrackingDatabase;
 
 string ConnectionString = @"Data Source = S4D08; Initial Catalog = AssetTracking; Integrated Security = True";
 
@@ -15,31 +16,30 @@ var Result = Command.ExecuteReader();
 
 Console.WriteLine("Asset Tracking");
 Console.WriteLine("--------------------------------------------------------------------------------------------------------------------");
-Console.WriteLine("ID".PadRight(10) + " |" + "Type".PadRight(15) + " |" + "Brand".PadRight(15) + " |" + "Model".PadRight(20) + " |" + "Office".PadRight(15) + " |" + "Purchase Date".PadRight(20) + " |" + "Price in USD".PadRight(10) + " |" + "Currency".PadRight(15) + " |" + "Local Price Today");
+Console.WriteLine("ID".PadRight(10) + " |" + "Type".PadRight(15) + " |" + "Brand".PadRight(15) + " |" + "Model".PadRight(20) + " |" + "Office".PadRight(15) + " |" + "Purchase Date".PadRight(20) + " |" + "Price in USD".PadRight(15) + " |" + "Currency".PadRight(15) + " |" + "Local Price Today");
 Console.WriteLine("--------------------------------------------------------------------------------------------------------------------");
+
+List<Products> orderedList = new List<Products>();
 
 while (Result.Read())
 {
 
-    var ProductID = Result["ProductID"].ToString();
-    var TypeProduct = Result["TypeProduct"].ToString();
-    var Brand = Result["Brand"].ToString();
-    var Model = Result["Model"].ToString();
-    var Office = Result["Office"].ToString();
-    var Purchase = Result["Purchase"].ToString();
-    var PriceByUSD = Result["PriceByUSD"].ToString();
-    var Currency = Result["Currency"].ToString();
-    var LocalPrice = Result["LocalPrice"].ToString();
+    Products newProd = new Products();
 
-    Console.WriteLine(ProductID?.PadRight(10) + " |" + TypeProduct?.PadRight(15) + " |" + Brand?.PadRight(15) + " |" + Model?.PadRight(20) + " |" + Office?.PadRight(15) + " |" + Purchase?.PadRight(20) + " |" + PriceByUSD?.PadRight(10) + " |" + Currency?.PadRight(15) + " |" + LocalPrice?.PadRight(5));
+    newProd.ProductID = Convert.ToInt32(Result["ProductID"].ToString());
+    newProd.TypeProduct = Result["TypeProduct"].ToString();
+    newProd.Brand = Result["Brand"].ToString();
+    newProd.Model = Result["Model"].ToString();
+    newProd.Office = Result["Office"].ToString();
+    newProd.Purchase = Convert.ToDateTime(Result["Purchase"].ToString());
+    newProd.PriceByUSD = Convert.ToInt32(Result["PriceByUSD"].ToString());
+    newProd.Currency = Result["Currency"].ToString();
+    newProd.LocalPrice = Convert.ToInt32(Result["LocalPrice"].ToString());
+
+    orderedList.Add(newProd);
+    orderedList = orderedList.OrderBy(x => x.TypeProduct).ToList();
+
+    Console.WriteLine(newProd.ProductID.ToString().PadRight(10) + ": " + newProd.TypeProduct.PadRight(15) + ": " + newProd.Brand.PadRight(15) + ": " + newProd.Model.PadRight(20) + ": " + newProd.Office.PadRight(15) + ": " + newProd.Purchase.ToString("yyyy-MM-dd").PadRight(20) + ": " + newProd.PriceByUSD.ToString().PadRight(15) + ": " + newProd.Currency.PadRight(15) + ":" + newProd.LocalPrice);
 }
 
-
 Connection.Close();
-
-
-
-
-
-
-
