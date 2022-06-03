aa66e53b88ab8f4c0c6d629365393008d4eddd73
Jetpreeya Yawichai <84719740+Jetpreeya@users.noreply.github.com>
2022-06-03 12:15:32 +0200

    Asset with Database is done.

--- AssetTrackingDatabase/AssetTrackingDatabase/Program.cs
+++ AssetTrackingDatabase/AssetTrackingDatabase/Program.cs
@@ -15,9 +15,9 @@ SqlCommand Command = new SqlCommand("select * from Products", Connection);
 var Result = Command.ExecuteReader();
 
 Console.WriteLine("Asset Tracking");
-Console.WriteLine("--------------------------------------------------------------------------------------------------------------------");
+Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
 Console.WriteLine("ID".PadRight(10) + " |" + "Type".PadRight(15) + " |" + "Brand".PadRight(15) + " |" + "Model".PadRight(20) + " |" + "Office".PadRight(15) + " |" + "Purchase Date".PadRight(20) + " |" + "Price in USD".PadRight(15) + " |" + "Currency".PadRight(15) + " |" + "Local Price Today");
-Console.WriteLine("--------------------------------------------------------------------------------------------------------------------");
+Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
 
 List<Products> orderedList = new List<Products>();
 
@@ -39,14 +39,15 @@ while (Result.Read())
     orderedList.Add(newProd);
 
     Console.WriteLine(newProd.ProductID.ToString().PadRight(10) + ": " + newProd.TypeProduct.PadRight(15) + ": " + newProd.Brand.PadRight(15) + ": " + newProd.Model.PadRight(20) + ": " + newProd.Office.PadRight(15) + ": " + newProd.Purchase.ToString("yyyy-MM-dd").PadRight(20) + ": " + newProd.PriceByUSD.ToString().PadRight(15) + ": " + newProd.Currency.PadRight(15) + ":" + newProd.LocalPrice);
-    
 }
 
 //Order by type
 orderedList = orderedList.OrderBy(x => x.TypeProduct).ToList();
-Console.WriteLine("----------------------------------------------------------------------------------------------------------");
+Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
 Console.WriteLine("Sort List by Type");
-Console.WriteLine("----------------------------------------------------------------------------------------------------------");
+Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+Console.WriteLine("ID".PadRight(10) + " |" + "Type".PadRight(15) + " |" + "Brand".PadRight(15) + " |" + "Model".PadRight(20) + " |" + "Office".PadRight(15) + " |" + "Purchase Date".PadRight(20) + " |" + "Price in USD".PadRight(15) + " |" + "Currency".PadRight(15) + " |" + "Local Price Today");
+Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
 
 foreach (Products b in orderedList)
 {
@@ -54,7 +55,52 @@ foreach (Products b in orderedList)
     Console.ResetColor();
 }
 
+//Level 2 
+List<Products> sortedDate = orderedList.OrderBy(any => any.Purchase).ToList();
+Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+Console.WriteLine("Sort List by Date ");
+Console.WriteLine("Mark any item *RED* if purchase date is less than 3 months away from 3 years.");
+Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+Console.WriteLine("ID".PadRight(10) + " |" + "Type".PadRight(15) + " |" + "Brand".PadRight(15) + " |" + "Model".PadRight(20) + " |" + "Office".PadRight(15) + " |" + "Purchase Date".PadRight(20) + " |" + "Price in USD".PadRight(15) + " |" + "Currency".PadRight(15) + " |" + "Local Price Today");
+Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
 
+foreach (Products b in sortedDate) // Mark any item *RED* if purchase date is less than 3 months away from 3 years.
+{
+    bool test = b.Purchase > DateTime.Now.AddYears(-3).AddMonths(3);
+    if (test) Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(b.ProductID.ToString().PadRight(10) + " |" + b.TypeProduct.PadRight(15) + " |" + b.Brand.PadRight(15) + " |" + b.Model.PadRight(20) + " |" + b.Office.PadRight(15) + " |" + b.Purchase.ToString("yyyy-MM-dd").PadRight(20) + " |" + b.PriceByUSD.ToString().PadRight(15) + " |" + b.Currency.PadRight(15) + " |" + b.LocalPrice);
+    Console.ResetColor();
+}
+
+Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+Console.WriteLine("Mark any item *Yellow* if date less than 6 months away from 3 years.");
+Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+Console.WriteLine("ID".PadRight(10) + " |" + "Type".PadRight(15) + " |" + "Brand".PadRight(15) + " |" + "Model".PadRight(20) + " |" + "Office".PadRight(15) + " |" + "Purchase Date".PadRight(20) + " |" + "Price in USD".PadRight(15) + " |" + "Currency".PadRight(15) + " |" + "Local Price Today");
+Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+foreach (Products b in sortedDate) // Items *Yellow* if date less than 6 months away from 3 years
+{
+    bool test = b.Purchase > DateTime.Now.AddYears(-6).AddMonths(3);
+    if (test) Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine(b.ProductID.ToString().PadRight(10) + " |" + b.TypeProduct.PadRight(15) + " |" + b.Brand.PadRight(15) + " |" + b.Model.PadRight(20) + " |" + b.Office.PadRight(15) + " |" + b.Purchase.ToString("yyyy-MM-dd").PadRight(20) + " |" + b.PriceByUSD.ToString().PadRight(15) + " |" + b.Currency.PadRight(15) + " |" + b.LocalPrice);
+    Console.ResetColor();
+}
+
+//Filtered by currency Level 3 
+
+List<ExchangeRate> showCurrency = new List<ExchangeRate>();
+ExchangeRate c1 = new ExchangeRate( "EUR", 801.65);
+ExchangeRate c2 = new ExchangeRate( "SEK", 50.65);
+showCurrency.Add(c1);
+showCurrency.Add(c2);
+
+// List<ExchangeRate> filteredCurrency = showCurrency.Where(c => c.Currency == "SEK").ToList();
+Console.WriteLine("-----------------------------------");
+Console.WriteLine("Currency".PadRight(15) + "|" + "Rate".PadRight(10));
+Console.WriteLine("-----------------------------------");
+foreach (ExchangeRate curren1 in showCurrency)
+{
+    Console.WriteLine(curren1.Currency.PadRight(15) + curren1.Rate.ToString().PadRight(15));
+}
 
 Connection.Close();
-
