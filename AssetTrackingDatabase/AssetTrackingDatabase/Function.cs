3f52bcf8e5440fd898e99df38ad7c3107ab1802e
Jetpreeya Yawichai <84719740+Jetpreeya@users.noreply.github.com>
2022-06-03 10:33:06 +0200

    Update Function.cs

--- AssetTrackingDatabase/AssetTrackingDatabase/Function.cs
+++ AssetTrackingDatabase/AssetTrackingDatabase/Function.cs
@@ -1,25 +1,22 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace AssetTrackingDatabase
 {
     public class Products
     {
-        public string Type { get; set; }
+        public int ProductID { get; set; }
+        public string TypeProduct { get; set; }
         public string Brand { get; set; }
         public string Model { get; set; }
         public string Office { get; set; }
-        public DateTime Year { get; set; }
-        public int Price { get; set; }
+        public DateTime Purchase { get; set; }
+        public int PriceByUSD { get; set; }
         public string Currency { get; set; }
         public double LocalPrice { get; set; }
-        public string Country { get; set; }
     }
 
 }
-
-
