6484ccb5b0504a7d12aa7c5ed0f0b1012dd3b5d6
Jetpreeya Yawichai <84719740+Jetpreeya@users.noreply.github.com>
2022-06-09 12:36:37 +0200

    Web API V.6

---
+++ WebApi/WebApi/Car.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApi
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+
+
+        public Car(int id, string brand, string model)
+        {
+            Id = id;
+            Brand = brand;
+            Model = model;
+        }
+
+
+        public Car() { }
+    }
+}
---
+++ WebApi/WebApi/Controllers/CarsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CarsController
+    {
+        List<Car> CarData;
+
+        public CarsController()
+        {
+            CarData = new List<Car>();
+            CarData.Add(new Car { Id = 1, Brand = "Mercedes", Model = "S600" });
+            CarData.Add(new Car { Id = 2, Brand = "Volvo", Model = "245" });
+            CarData.Add(new Car { Id = 3, Brand = "SAAB", Model = "9000" });
+            CarData.Add(new Car { Id = 4, Brand = "Audi", Model = "A6" });
+            CarData.Add(new Car { Id = 5, Brand = "BMW", Model = "650" });
+            CarData.Add(new Car { Id = 6, Brand = "Citroen", Model = "DS5" });
+            CarData.Add(new Car { Id = 7, Brand = "Mercedes", Model = "SL500" });
+            CarData.Add(new Car { Id = 8, Brand = "BMW", Model = "M5" });
+            CarData.Add(new Car { Id = 9, Brand = "Porsche", Model = "S4 Carrera" });
+            CarData.Add(new Car { Id = 10, Brand = "Lexus", Model = "400" });
+            CarData.Add(new Car { Id = 11, Brand = "Toyota", Model = "Corolla" });
+        }
+
+
+        [HttpGet]
+        public List<Car> Index()
+        {
+            return CarData;
+        }
+
+        [HttpGet]
+        [Route("/getcar/{id}")]
+        public Car GetCar(int id)
+        {
+            Car Car = CarData.FirstOrDefault(x => x.Id == id); //Lamda expession (x => x.Id == id)
+            return Car;
+        }
+    }
+}
