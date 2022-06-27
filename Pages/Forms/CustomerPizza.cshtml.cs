14b2ec1b49e0043c43423f78e2487b0ded21ad51
Jetpreeya Yawichai <84719740+Jetpreeya@users.noreply.github.com>
2022-06-27 15:45:56 +0200

    Pizza Final project

---
+++ Pages/Forms/CustomerPizza.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinalPizzaProject.Pages.Forms
+{
+    public class CustomerPizzaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Pizza.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinalPizzaProject.Pages
+{
+    public class PizzaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
